7430b71314b9abb705af6c05560ed574ad65f00a
Kamran Khan <kamran@kage.com>
2020-02-10 15:28:28 -0500

    Hack: remove a reference to  in the compiled output for a quick SSR fix

--- dist/jspdf.debug.js
+++ dist/jspdf.debug.js
@@ -9050,7 +9050,7 @@
    *               2014 Diego Casorran, https://github.com/diegocr
    *               2014 James Robb, https://github.com/jamesbrobb
    *
-   * 
+   *
    */
 
   /**
@@ -10835,7 +10835,7 @@
    *               2014 James Hall, james@parall.ax
    *               2014 Diego Casorran, https://github.com/diegocr
    *
-   * 
+   *
    * ====================================================================
    */
 
@@ -14551,7 +14551,7 @@
    * ====================================================================
    * Copyright (c) 2013 Youssef Beddad, youssef.beddad@gmail.com
    *
-   * 
+   *
    * ====================================================================
    */
 
@@ -14957,7 +14957,7 @@
    *
    * Copyright (c) 2014 James Robb, https://github.com/jamesbrobb
    *
-   * 
+   *
    * ====================================================================
    */
 
@@ -15774,7 +15774,7 @@
    * Copyright (c) 2012 Willow Systems Corporation, willow-systems.com
    *               2014 Diego Casorran, https://github.com/diegocr
    *
-   * 
+   *
    * ====================================================================
    */
 
@@ -16118,7 +16118,7 @@
    jsPDF standard_fonts_metrics plugin
    * Copyright (c) 2012 Willow Systems Corporation, willow-systems.com
    * MIT license.
-   * 
+   *
    * ====================================================================
    */
 
@@ -16441,7 +16441,7 @@
   /** @license
    * Copyright (c) 2012 Willow Systems Corporation, willow-systems.com
    *
-   * 
+   *
    * ====================================================================
    */
 
@@ -16650,7 +16650,7 @@
    * ====================================================================
    * Copyright (c) 2013 Eduardo Menezes de Morais, eduardo.morais@usp.br
    *
-   * 
+   *
    * ====================================================================
    */
 
@@ -17051,7 +17051,7 @@
    * jsPDF XMP metadata plugin
    * Copyright (c) 2016 Jussi Utunen, u-jussi@suomi24.fi
    *
-   * 
+   *
    * ====================================================================
    */
 
@@ -17371,10 +17371,10 @@
                       if (Object.prototype.toString.call(text[s]) === '[object Array]') {
                           cmapConfirm = fonts[key].metadata.cmap.unicode.codeMap[strText[s][0].charCodeAt(0)]; //Make sure the cmap has the corresponding glyph id
                       } else {
-                          
+
                       }
                   //}
-                  
+
               } else {
                   cmapConfirm = fonts[key].metadata.cmap.unicode.codeMap[strText[s].charCodeAt(0)]; //Make sure the cmap has the corresponding glyph id
               }*/
@@ -17748,7 +17748,7 @@
    *               2014 Steven Spungin, https://github.com/flamenco
    *
    * @license
-   * 
+   *
    * ====================================================================
    */
   (function (jsPDFAPI) {
@@ -21038,25 +21038,25 @@
     Copyright (c) 2008, Adobe Systems Incorporated
     All rights reserved.
 
-    Redistribution and use in source and binary forms, with or without 
+    Redistribution and use in source and binary forms, with or without
     modification, are permitted provided that the following conditions are
     met:
 
-    * Redistributions of source code must retain the above copyright notice, 
+    * Redistributions of source code must retain the above copyright notice,
       this list of conditions and the following disclaimer.
-    
+
     * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the 
+      notice, this list of conditions and the following disclaimer in the
       documentation and/or other materials provided with the distribution.
-    
-    * Neither the name of Adobe Systems Incorporated nor the names of its 
-      contributors may be used to endorse or promote products derived from 
+
+    * Neither the name of Adobe Systems Incorporated nor the names of its
+      contributors may be used to endorse or promote products derived from
       this software without specific prior written permission.
 
     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
     IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
     THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
-    PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR 
+    PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
     CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
     EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
     PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
@@ -26348,7 +26348,7 @@
 
     new WebPDecoder();
     /*Copyright (c) 2017 Dominik Homberger
-    
+
     https://webpjs.appspot.com
     WebPRiffParser dominikhlbg@gmail.com
     */
@@ -26538,8 +26538,8 @@
    1. Redistributions of source code must retain the above copyright notice,
    this list of conditions and the following disclaimer.
 
-   2. Redistributions in binary form must reproduce the above copyright 
-   notice, this list of conditions and the following disclaimer in 
+   2. Redistributions in binary form must reproduce the above copyright
+   notice, this list of conditions and the following disclaimer in
    the documentation and/or other materials provided with the distribution.
 
    3. The names of the authors may not be used to endorse or promote products
@@ -30770,8 +30770,8 @@
   # PNG.js
   # Copyright (c) 2011 Devon Govett
   # MIT LICENSE
-  # 
-  # 
+  #
+  #
   */
   (function (global) {
     global.PNG = function () {
@@ -31354,7 +31354,7 @@
    *               Justin D'Arcangelo <justindarc@gmail.com>
    *               Yury Delendik
    *
-   * 
+   *
    */
   var DecodeStream = function () {
     function constructor() {
@@ -31729,7 +31729,7 @@
       factory();
       global.FileSaver = mod.exports;
     }
-  })(window, function () {
+  })(typeof window === "undefined" ? {} : window, function () {
     /*
      * FileSaver.js
      * A saveAs() FileSaver implementation.
